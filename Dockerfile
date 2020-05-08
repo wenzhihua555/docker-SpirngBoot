64805143bd8da43d1e2e57a8037de2ddeed2dd58
文志华 <wenzhihua@jsdttec.com>
2020-05-08 15:06:45 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM codenvy/jdk8_maven3_tomcat8
+
+VOLUME /tmp
+
+ADD target/demo-0.0.1-SNAPSHOT.jar app.jar
+
+#RUN sh -c 'touch /app.jar'
+
+ENTRYPOINT [ "sh", "-c", "java  -jar /app.jar" ]
+
---
+++ src/main/java/com/docker/demo/controller/DockerController.java
@@ -0,0 +1,20 @@
+package com.docker.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@Slf4j
+@RequestMapping("/docker")
+public class DockerController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello docker";
+    }
+}
